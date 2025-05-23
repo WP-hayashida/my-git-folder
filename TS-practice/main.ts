ce827bc81ee1417544d1d30384bb3ce21deb096b
hayashida <hayashida@workport.co.jp>
2025-05-23 12:07:27 +0900

    TS-practice追加

---
+++ TS-practice/main.ts
@@ -0,0 +1,4 @@
+import { User } from "./models/User";
+
+const user = new User(1, "Taro");
+user.greet();
---
+++ TS-practice/models/User.ts
@@ -0,0 +1,9 @@
+import { User as IUser } from "..//interfaces/User";
+
+export class User implements IUser {
+  constructor(public id: number, public name: string) {}
+
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+}
