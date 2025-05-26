16edc8c8d3064c4ce7991e3ad3f4ffe7198ae11d
hayashida <hayashida@workport.co.jp>
2025-05-26 15:39:18 +0900

    猫画像

---
+++ vite-project/like-button/random-cat/app/page.tsx
@@ -0,0 +1,8 @@
+import { fetchImage } from "./fetch-image";
+
+import { CatImage } from "./cat-image";
+
+export default async function Home() {
+  const image = await fetchImage();
+  return <CatImage url={image.url} />;
+}
